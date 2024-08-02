d1353aa5abc6270d7216aa65a983d04730afa80a
MARWANANO32 <marwanano32@gmail.com>
2024-08-03 00:13:49 +0300

    MARWAN MOSTAFA

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,5 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
-
-const students = getListStudents();
-
-console.log(getStudentsByLocation(students, 'San Francisco'));
+function getStudentsByLocation(students, city) {
+    return students.filter((student) => student.location === city);
+  }
+  
+  export default getStudentsByLocation;
