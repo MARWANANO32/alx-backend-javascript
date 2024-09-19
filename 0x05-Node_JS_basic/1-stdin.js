c21ea6bee263662b5e84403a05d614ba2e89e887
MARWANANO32 <marwanano32@gmail.com>
2024-09-19 08:07:57 +0300

    MARWAN MOSTAFA

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 // seond message that will take the input from the user
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
-    process.stdout.write("your name is: ${name}\n");
+    process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
 });
 // Third message that will close the process after the user input
