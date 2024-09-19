27e4f913f096246ce7e0ca5f295242052d4077e0
MARWANANO32 <marwanano32@gmail.com>
2024-09-19 06:25:02 +0300

    MARWAN MOSTAFA

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,14 @@
-const displayMessage = (message) => {
-  console.log("STDOUT: " + message);
-}
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log("STDOUT: " + message);
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
