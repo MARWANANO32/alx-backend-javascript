ae5a3542ed270f26456b6cb53d9c08e0dfc0c7e4
MARWANANO32 <marwanano32@gmail.com>
2024-09-19 08:20:54 +0300

    MARWAN MOSTAFA

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,16 @@
-// first process that out the message to user
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// seond message that will take the input from the user
-if(process.stdin.isTTY){
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name.toString()}\n`);
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-});
-}
-else{
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name.toString()}\n`);
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
+  });
 }
-// Third message that will close the process after the user input
 process.on('exit', () => {
-    console.log("This important software is now closing\n");
+   console.log('This important software is now closing');
 });
